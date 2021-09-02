95e05b05cc14c2068e1dff5a6a4e98c31054ed7b
Mauricio De Armas <mdg1007@alu.ubu.es>
2021-09-02 12:47:38 +0200

    Access to the users and Readme Update

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
---> To start
+--> To start:
+**Install Dependencies**
+```
+# Linux/macOS
+python3 -m pip install -U discord.py
+
+# Windows
+py -3 -m pip install -U discord.py
+```
 
 **Add Discord API Key**
+**Run the bot**
 ```
 python3 src/index.py
 ```
--- src/index.py
+++ src/index.py
@@ -1,21 +1,37 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 import config
 
 
 api_key = config.api_key
+
 #Command type: >test
-bot = commands.Bot(command_prefix=">")
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="", intents=intents)
+
 
 
 @bot.event
+
 async def on_ready():
     print("BotReady")
+    #users = discord.Guild.users
+    roles = discord.Guild.roles
+    guild = bot.get_guild(id=882606921780199475)
+    members = guild.members
+    print(guild.roles)
+    async for member in guild.fetch_members(limit=150):
+        if member.name != 'PisoBot' and list(guild.roles)[1] in member.roles:
+            print(member.name)
+            print(list(guild.roles)[1].name)
+
 
 
 @bot.command()
 async def test(ctx):
     print("Online")
-    await ctx.send('the bot correct')
+    await ctx.send('the bot works correct')
 
 bot.run(api_key)
